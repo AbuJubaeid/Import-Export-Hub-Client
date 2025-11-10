d2366aa8fbcb5e7db45a5ac85dfcf1e50a163d2f
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-11-11 01:13:15 +0600

    google login added in registration page

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -7,7 +7,7 @@ import { AuthContext } from "../../AuthContext/AuthContext";
 
 const Signup = () => {
   const [show, setShow] = useState(false);
-  const { createUserWithEmailAndPasswordFunc, updateProfileFunc, setLoading, signOutFunc, setUser } =
+  const { createUserWithEmailAndPasswordFunc, updateProfileFunc, setLoading, signOutFunc, setUser, signInWithPopupFunc } =
     useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -82,6 +82,20 @@ const Signup = () => {
     e.target.reset();
   };
 
+  const handleGoogleBtn =()=>{
+    signInWithPopupFunc()
+    .then(result=>{
+        console.log(result)
+        navigate("/");
+        setLoading(false)
+        toast.success("Login successful");
+    })
+    .catch((error) => {
+        console.log(error);
+        toast.error(error.message);
+      });
+  }
+
   return (
     <div className="hero bg-base-200 min-h-screen mt-10 mb-10">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -134,6 +148,39 @@ const Signup = () => {
               <button className="btn btn-neutral mt-4">Register</button>
             </fieldset>
           </form>
+          
+          {/* google login */}
+          <button onClick={handleGoogleBtn} className="btn mx-6 mb-4 bg-white text-black border-[#e5e5e5]">
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+            Login with Google
+          </button>
+
           <p className="text-center mb-2 text-gray-600">
             Already have an account?{" "}
             <Link
